ea35be39ac9debac1adcbc35f059602d336f929e
Codenerd1 <muhiageorge98@gmail.com>
2024-02-28 08:39:54 -0800

    Finding the area and perimeter of a rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,11 +1,50 @@
-i!/usr/bin/python3                                           """Defines a Rectangle class."""                                                                                          class Rectangle:                                                 """Represent a rectangle."""                                                                                              def __init__(self, width=0, height=0):                           """Initialize a new rectangle.                               Args:                                                            width(int): Width of the new rectangle.                      height(int): Height of the new rectanle.                     """                                                      self.width = width                                           self.height = height                                                                                                  @property                                                    def width(self):                                                 """Set the width of the rectangle."""                        return self.__width                                                                                                   @width.setter                                                def width(self, value):                                          if not isinstance(value, int):                                   raise TypeError("width must be an integer")              if value < 0:                                                    raise ValueError("width must be >= 0")                   self.__width = value                                                                                                  @property                                                    def height(self):                                                """Set the height of the rectangle"""                        return self.__height                                                                                                  @height.setter                                               def height(self, value):                                         if not isinstance(value, int):                                   raise TypeError("height must be an integer")             if value < 0:                                                    raise ValueError("height must be >= 0")                  self.__height = value
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
 
-    def area(self):
-        'Returns the area of the rectangle'
-        return (self._width * self._height)
 
-    def perimeter(self):
-        'Returns the perimeter of the rectangle'
-        if self._width == 0 or self._height == 0:
-            return (0)
-        return ((self._width * 2) + (self._height * 2))
+class Rectangle:
+    """Represent a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new rectangle.
+        Args:
+            width(int): Width of the new rectangle.
+            height(int): Height of the new rectanle."""
+        self.width = width
+        self.height = height
+
+        @property
+        def width(self):
+            """Set the width of the rectangle."""
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            if not isinstance(value, int):
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = value
+
+        @property
+        def height(self):
+            """Set the height of the rectangle"""
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            if not isinstance(value, int):
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
+
+        def area(self):
+            'Returns the area of the rectangle'
+            return (self._width * self._height)
+
+        def perimeter(self):
+            'Returns the perimeter of the rectangle'
+            if self._width == 0 or self._height == 0:
+                return (0)
+            return ((self._width * 2) + (self._height * 2))
