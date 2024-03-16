d01ab4479547e7844eabe1379b8a8efe5436bf8f
Codenerd1 <muhiageorge98@gmail.com>
2024-03-15 18:55:42 -0700

    Revision for Task 5 python file

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -19,7 +19,7 @@ def matrix_divided(matrix, div):
         not all(isinstance(row, list) for row in matrix) or
         not all((isinstance(ele, int) or isinstance(ele, float))
                 for ele in [num for row in matrix for num in row]))
-    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    raise TypeError("matrix must be a matrix (list of lists) of int/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
@@ -29,4 +29,4 @@ def matrix_divided(matrix, div):
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
+    return [list(map(lambda x: round(x / div, 2), row)) for row in matrix]
--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -4,17 +4,18 @@
 
 
 def max_integer(list=[]):
-        """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-        """
-        if len(list) == 0:
-            return None
-        result = list[0]
-        i = 1
-        while i < len(list):
-            if list[i] > result:
+    """Function to find and return the max integer in a list of integers
+    If the list is empty, the function returns None
+    """
+    if len(list) == 0:
+        return None
 
-                result = list[i]
-                i += 1
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
 
-        return result
+            result = list[i]
+            i += 1
+
+    return result
