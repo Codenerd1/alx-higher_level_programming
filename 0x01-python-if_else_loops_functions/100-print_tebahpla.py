f605a0c43f4cec5630f3a934f534c4a73bc7e40a
Codenerd1 <muhiageorge98@gmail.com>
2024-04-12 09:16:36 -0700

    Third revision for task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(122, 64, -1):
     if i >= 97:  # ASCII value of 'a'
-        print("{0}".format(chr(i) if (i - 122) % 2 == 0 else chr(i - 32)), end='')
+        print("{0}".format(chr(i) if (i - 122) % 2 == 0
+    else chr(i - 32)), end='')
