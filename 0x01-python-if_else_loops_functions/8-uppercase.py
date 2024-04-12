7acba655ba8c3bf2350a449eae029b47eb7628ef
Codenerd1 <muhiageorge98@gmail.com>
2024-04-12 04:45:46 -0700

    Revision for task four on decimal and hexadecimals

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for char in str:
         ascii_value = ord(char)
         if 97 <= ascii_value <= 122:
             ascii_value -= 32
-        print(chr(ascii_value), end="")
-    print()
+        result += chr(ascii_value)
+    return result
