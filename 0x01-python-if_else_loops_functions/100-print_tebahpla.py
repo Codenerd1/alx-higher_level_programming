0b9afecf081f20ca7220b48fce32d12b0547b553
Codenerd1 <muhiageorge98@gmail.com>
2024-04-12 09:14:18 -0700

    Second revision for task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-i = 122
-while i >= 65:
-    if i >= 97:
-        print(f"{chr(i) if (i - 122) % 2 == 0 else chr(i - 32)}", end='')
-    i -= 1
+for i in range(122, 64, -1):
+    if i >= 97:  # ASCII value of 'a'
+        print("{0}".format(chr(i) if (i - 122) % 2 == 0 else chr(i - 32)), end='')
