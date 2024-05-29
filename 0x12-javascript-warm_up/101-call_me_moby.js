206f9c2bb98f4e7ac94e24168ecb1a1e38ddb4e5
Codenerd1 <muhiageorge98@gmail.com>
2024-05-29 13:47:22 -0700

    A function that executes x times a function without using var

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Define the function that executes another function x times
+function callMeMoby(x, theFunction) {
+  // Use a loop to execute theFunction x times
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+}
+
+// Export the function to be visible from outside
+module.exports.callMeMoby = callMeMoby;
