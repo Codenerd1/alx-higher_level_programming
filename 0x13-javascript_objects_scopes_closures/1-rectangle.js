954ce3009de60704f4b8cf914297853b1ff5ba3c
Codenerd1 <muhiageorge98@gmail.com>
2024-06-02 15:09:04 -0700

    A class Rectangle that defines a rectangle with two arguments, weidth (w) and height (h)

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// A class Rectangle that takes two arguments, width (w) and height (h)
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
