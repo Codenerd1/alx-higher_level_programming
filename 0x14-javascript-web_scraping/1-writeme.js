7ebc6ae46c5f847527e89ce3dad9b51a2111396b
Codenerd1 <muhiageorge98@gmail.com>
2024-06-04 12:59:39 -0700

    Revision for task 1: A script that writes a string to a file using utf-8, the first argument is the file path, the second is the string to write

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,11 +5,6 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-if (!filePath || !content) {
-  console.error(err);
-  process.exit(1);
-}
-
 // Write the content of the file in UTF-8 encoding
 fs.writeFile(filePath, content, 'utf8', (err) => {
   if (err) {
