06a086bc2f370bc56355e597a4783b5595518c7b
Codenerd1 <muhiageorge98@gmail.com>
2024-06-04 13:30:23 -0700

    A script that display the status code of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,16 +4,11 @@ const request = require('request');
 // Get the URL to request from the command-line arguments
 const url = process.argv[2];
 
-if (!url) {
-  console.error('Please provide the URL to request as first argument.');
-  process.exit(1);
-}
-
 // Send a GET request to the specified URL
-request.get(url, (error, response) => {
+request(url, (error, response) => {
   if (error) {
-    console.error(error);
+    console.error('code:', error);
   } else {
-    console.log(response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
