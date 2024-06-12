441a37088d913225a4fdd2b7d1753f42b20805ea
Codenerd1 <muhiageorge98@gmail.com>
2024-06-12 16:17:17 -0700

    A script that imports an array and computes a new array where each value is multiplied by its index

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const { list } = require('./100-data');
+
+// Compute the new array
+const newList = list.map((value, index) => value * index);
+
+// Print both the initial and new list
+console.log('Initial list:', list);
+console.log('New list:', newList);
