7d25c46024c8a1c946f47f826ba1aa4801355076
Codenerd1 <muhiageorge98@gmail.com>
2024-06-12 17:22:45 -0700

    script that concats 2 files.
    The first argument is the file path of the first source file
    The second argument is the file path of the second source file
    The third argument is the file path of the destination

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Get command line arguments
+const [, , sourceFile1, sourceFile2, destinationFile] = process.argv;
+
+// Read content of file1
+fs.readFile(sourceFile1, 'utf8', (err, data1) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  // Read content of file2
+  fs.readFile(sourceFile2, 'utf8', (err, data2) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    // Concatenate the contents
+    const concatenatedContent = data1.trim() + '\n' + data2.trim() + '\n';
+
+    // Write to destination file
+    fs.writeFile(destinationFile, concatenatedContent, 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+  });
+});
+
